741916a9863229266573af5dedffe6d78dbc6f01
Aishwarya-Varma123 <aishwaryavarma26@gmail.com>
2025-04-02 17:14:34 +0530

    Fixed duplicate app.run issue

--- HALO/app.py
+++ HALO/app.py
@@ -65,7 +65,4 @@ def clear():
     return jsonify({"message": "Cleared"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000, debug=True)
